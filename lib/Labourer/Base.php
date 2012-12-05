58705158935882a716bc6a04c79ab094108821bf
Alvaro Cabrera <pateketrueke@gmail.com>
2012-12-05 10:59:01 -0600

    Do not initialize under CLI

--- lib/Labourer/Base.php
+++ lib/Labourer/Base.php
@@ -7,31 +7,35 @@ class Base
 
   public static function initialize()
   {
-    // method override
-    if ($_method = (isset($_POST['_method']) ? $_POST['_method'] : FALSE)) {
-      $_SERVER['REQUEST_METHOD'] = strtoupper($_method);
-      unset($_POST['_method']);
+    $test = strtoupper(PHP_SAPI);
+
+    if ((strpos($test, 'CLI') === FALSE) OR ($test === 'CLI-SERVER')) {
+      // method override
+      if ($_method = (isset($_POST['_method']) ? $_POST['_method'] : FALSE)) {
+        $_SERVER['REQUEST_METHOD'] = strtoupper($_method);
+        unset($_POST['_method']);
+      }
+
+
+      // CSRF override
+      if ($_token = (isset($_POST['_token']) ? $_POST['_token'] : FALSE)) {
+        $_SERVER['HTTP_X_CSRF_TOKEN'] = $_POST['_token'];
+        unset($_POST['_token']);
+      }
+
+
+      // security
+      \Labourer\Web\Session::initialize();
+
+      $salt = \Labourer\Config::get('csrf_salt');
+      $token = time() . ' ' . md5($salt . uniqid(session_id()));
+      $check = ! empty($_SESSION['__CSRF_TOKEN']) ? $_SESSION['__CSRF_TOKEN'] : '';
+
+      \Labourer\Config::set('csrf_check', $check);
+      \Labourer\Config::set('csrf_token', $token);
+
+      $_SESSION['__CSRF_TOKEN'] = $token;
     }
-
-
-    // CSRF override
-    if ($_token = (isset($_POST['_token']) ? $_POST['_token'] : FALSE)) {
-      $_SERVER['HTTP_X_CSRF_TOKEN'] = $_POST['_token'];
-      unset($_POST['_token']);
-    }
-
-
-    // security
-    \Labourer\Web\Session::initialize();
-
-    $salt = \Labourer\Config::get('csrf_salt');
-    $token = time() . ' ' . md5($salt . uniqid(session_id()));
-    $check = ! empty($_SESSION['__CSRF_TOKEN']) ? $_SESSION['__CSRF_TOKEN'] : '';
-
-    \Labourer\Config::set('csrf_check', $check);
-    \Labourer\Config::set('csrf_token', $token);
-
-    $_SESSION['__CSRF_TOKEN'] = $token;
   }
 
 }
